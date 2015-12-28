a80920eaebc13d2dcccac080ee7731483b421b21
378999587@qq.com <378999587@qq.com>
2015-12-28 14:35:14 +0800

    mux

---
+++ README.md
@@ -0,0 +1,5 @@
+# mux
+
+对 gorilla/mux 进一步封装,功能简单,只提供了 Prefix Get Post Delete Put 几个方法
+
+使用
---
+++ router.go
@@ -0,0 +1,46 @@
+package mux
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Router struct {
+	*mux.Router
+}
+
+func New() *Router {
+	r := Router{mux.NewRouter()}
+	return &r
+}
+
+func (r *Router) AddFunc(path string, method string, f func(*Context)) *mux.Route {
+	return r.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		context := Context{w, r}
+		f(&context)
+	}).Methods(method)
+}
+
+func (r *Router) Prefix(prefix string, route ...mux.Route) {
+	for _, mr := range route {
+		mr.PathPrefix(prefix)
+	}
+}
+
+func (r *Router) Get(path string, f func(*Context)) *mux.Route {
+	return r.AddFunc(path, "GET", f)
+}
+
+func (r *Router) Post(path string, f func(*Context)) *mux.Route {
+	return r.AddFunc(path, "POST", f)
+}
+
+func (r *Router) Delete(path string, f func(*Context)) *mux.Route {
+	return r.AddFunc(path, "DELETE", f)
+}
+
+func (r *Router) Put(path string, f func(*Context)) *mux.Route {
+	return r.AddFunc(path, "PUT", f)
+}
+
+
