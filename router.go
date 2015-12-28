457a5e1ee0418525f31b87aaea690bca2783ff79
378999587@qq.com <378999587@qq.com>
2015-12-28 18:08:17 +0800

    mux

--- router.go
+++ router.go
@@ -8,37 +8,23 @@ import (
 
 type Router struct {
 	*mux.Router
-	group string
 }
 
 func New() *Router {
-	r := Router{mux.NewRouter(),""}
+	r := Router{mux.NewRouter()}
 	return &r
 }
 
 func (r *Router) AddFunc(path string, method string, f func(*ctx.Context)) *mux.Route {
-	var s *mux.Router
-	if len(r.group) > 0 {
-		s = r.PathPrefix(r.group).Subrouter()
-	}
-	s = r.Router
-
-	return s.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
+	return r.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
 		context := ctx.Context{w, req}
 		f(&context)
 	}).Methods(method)
 }
 
-func (r *Router) Group(pattern string, mroute ...*mux.Route) error {
-	r.group = pattern
-	for _, mr := range mroute {
-		err := mr.GetError()
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+func (r *Router) Group(prefix string) *Router {
+	s := r.PathPrefix(prefix).Subrouter()
+	return &Router{s}
 }
 
 func (r *Router) Get(path string, f func(*ctx.Context)) *mux.Route {
