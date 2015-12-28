1447e238817cd1899e3c975ea7ed23c82fe6fe34
378999587@qq.com <378999587@qq.com>
2015-12-28 14:57:41 +0800

    mux

--- router.go
+++ router.go
@@ -1,6 +1,7 @@
 package mux
 
 import (
+	ctx "github.com/num5/context"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,9 +15,9 @@ func New() *Router {
 	return &r
 }
 
-func (r *Router) AddFunc(path string, method string, f func(*Context)) *mux.Route {
+func (r *Router) AddFunc(path string, method string, f func(*ctx.Context)) *mux.Route {
 	return r.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		context := Context{w, r}
+		context := ctx.Context{w, r}
 		f(&context)
 	}).Methods(method)
 }
@@ -27,19 +28,19 @@ func (r *Router) Prefix(prefix string, route ...mux.Route) {
 	}
 }
 
-func (r *Router) Get(path string, f func(*Context)) *mux.Route {
+func (r *Router) Get(path string, f func(*ctx.Context)) *mux.Route {
 	return r.AddFunc(path, "GET", f)
 }
 
-func (r *Router) Post(path string, f func(*Context)) *mux.Route {
+func (r *Router) Post(path string, f func(*ctx.Context)) *mux.Route {
 	return r.AddFunc(path, "POST", f)
 }
 
-func (r *Router) Delete(path string, f func(*Context)) *mux.Route {
+func (r *Router) Delete(path string, f func(*ctx.Context)) *mux.Route {
 	return r.AddFunc(path, "DELETE", f)
 }
 
-func (r *Router) Put(path string, f func(*Context)) *mux.Route {
+func (r *Router) Put(path string, f func(*ctx.Context)) *mux.Route {
 	return r.AddFunc(path, "PUT", f)
 }
 
